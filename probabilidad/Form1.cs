faaa7e4c93ec8c97874ad3d7436d9c548bd64a5e
lucasserafin <51752311+lucasserafin@users.noreply.github.com>
2020-11-07 19:38:43 -0300

    cambio comit

--- probabilidad/Form1.cs
+++ probabilidad/Form1.cs
@@ -39,7 +39,7 @@ namespace probabilidad
 
         private void button1_Click(object sender, EventArgs e)
         {
-            //coment233
+            //coment2333
             double total3 = 0;
             foreach (DataGridViewRow row in dgw.Rows)
             {
